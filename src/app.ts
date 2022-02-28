f93db2c0e6bbdcf1ee16af466a6d6e79d941e52f
José Flávio <jotaftm@gmail.com>
2022-02-27 23:43:28 -0300

    feat: cors

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,19 @@ import { connectDatabase } from "./database";
 import { initializerRouter } from "./routes";
 import { handleError } from "./middlewares/errorMiddleware";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 connectDatabase();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
